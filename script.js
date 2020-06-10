42a82f98abf542d65a3daf044e78fff6cd79422a
Caio Ledesma <42016457+caioledesma@users.noreply.github.com>
2020-06-09 22:22:53 -0300

    adicionando comida da cobrinha

--- script.js
+++ script.js
@@ -8,7 +8,10 @@ snake[0]= {
 };
 
 let direction = "right";
-
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
 
 function criarBG() {
     context.fillStyle = "lightgreen";
@@ -22,15 +25,20 @@ function criarCobrinha(){
     }
 }
 
+
+function drawFood(){
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box);
+}
+
+document.addEventListener("keydown", update);
+
 function update(event){
     if(event.keyCode == 37 && direction != "right") direction = "left";
     if(event.keyCode == 38 && direction != "down") direction = "up";
     if(event.keyCode == 39 && direction != "left") direction = "right";
     if(event.keyCode == 40 && direction != "up") direction = "down";
 }
-
-document.addEventListener("keydown", update);
-
 
 
 function iniciarJogo(){
@@ -41,6 +49,7 @@ function iniciarJogo(){
 
     criarBG();
     criarCobrinha();
+    drawFood();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
